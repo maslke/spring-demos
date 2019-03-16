83281c0f0875eba88614b8bfaa4801d0fce7ccd2
maslke <maslke@outlook.com>
2019-03-16 15:44:34 +0800

    ThreadLocal

---
+++ src/main/java/com/maslke/spring/basic/SequenceNumber.java
@@ -0,0 +1,40 @@
+package com.maslke.spring.basic;
+
+public class SequenceNumber {
+    private static ThreadLocal<Integer> seqNum = new ThreadLocal<Integer>() {
+        @Override
+        public Integer initialValue() {
+            return 0;
+        }
+    };
+
+    public int getNextValue() {
+        seqNum.set(seqNum.get() + 1);
+        return seqNum.get();
+    }
+
+    public static void main(String[] args) {
+        SequenceNumber sn = new SequenceNumber();
+        TestClient c1 = new TestClient(sn);
+        TestClient c2 = new TestClient(sn);
+        TestClient c3 = new TestClient(sn);
+        c1.start();
+        c2.start();
+        c3.start();
+    }
+
+    private static class TestClient extends Thread {
+        private SequenceNumber sn;
+
+        public TestClient(SequenceNumber sn) {
+            this.sn = sn;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 3; i++) {
+                System.out.println("thread[" + Thread.currentThread().getName() + "] sn[" + sn.getNextValue() + "]");
+            }
+        }
+    }
+}
