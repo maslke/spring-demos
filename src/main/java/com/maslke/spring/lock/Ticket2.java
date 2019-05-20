1b1dc35415d6841a80b16ce290455392c1f4f174
maslke <maslke@outlook.com>
2019-05-20 14:27:38 +0800

    executors

---
+++ src/main/java/com/maslke/spring/lock/Ticket2.java
@@ -0,0 +1,38 @@
+package com.maslke.spring.lock;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Ticket2 implements Runnable {
+    private AtomicInteger number;
+
+    public Ticket2(int num) {
+        this.number = new AtomicInteger((num));
+    }
+
+    @Override
+    public  void run() {
+        while (this.number.intValue() > 0) {
+            try {
+                Thread.sleep(20);
+            }
+            catch (InterruptedException ex) {
+                System.out.println(ex.getMessage());
+            }
+            System.out.println(Thread.currentThread().getName() + ":" + this.number.decrementAndGet());
+        }
+
+
+    }
+}
+
+class TicketTest2 {
+    public static void main(String[] args) {
+        Ticket ticket = new Ticket(1000);
+        Executor executor = Executors.newFixedThreadPool(3);
+        executor.execute(ticket);
+        executor.execute(ticket);
+        executor.execute(ticket);
+    }
+}
